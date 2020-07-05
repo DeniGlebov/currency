152633e270529a823b4faadcc4eca99402ba9a15
DeniGlebov <deni.glebov@gmail.com>
2020-07-05 10:14:45 +0300

    homework16

--- src/tests/test_rate.py
+++ src/tests/test_rate.py
@@ -31,26 +31,26 @@ def test_latest_rates(client):
 
 
 def test_unauthorized_edit_rate(client):
-    url = reverse('rate:edit-rate', kwargs={'pk': '0199b4aa-11ab-4abe-98c5-7b86aa93b65e'})
+    url = reverse('rate:edit-rate', kwargs={'pk': '0f1cc5db-79d3-4c78-85b6-6ab34a5eb890'})
     response = client.get(url)
     assert response.status_code == 302
 
 
 def test_unauthorized_delete_rate(client):
-    url = reverse('rate:delete-rate', kwargs={'pk': '0199b4aa-11ab-4abe-98c5-7b86aa93b65e'})
+    url = reverse('rate:delete-rate', kwargs={'pk': '152c2d3d-6d86-4cf9-8343-5957ea3fa612'})
     response = client.get(url)
     assert response.status_code == 302
 
 
 def test_authorized_an_admin_edit_rate(admin_client):
-    url = reverse('rate:edit-rate', kwargs={'pk': '0199b4aa-11ab-4abe-98c5-7b86aa93b65e'})
+    url = reverse('rate:edit-rate', kwargs={'pk': '3f090a33-6073-40fa-9b57-f9e67387a3ac'})
     response = admin_client.get(url)
     assert response.status_code == 200
 
 
 def test_authorized_an_admin_delete_rate(admin_client):
     count = Rate.objects.all().count()
-    url = reverse('rate:delete-rate', kwargs={'pk': '0199b4aa-11ab-4abe-98c5-7b86aa93b65e'})
+    url = reverse('rate:delete-rate', kwargs={'pk': '3f5d6cba-7154-403c-81dd-cd0023f8ee86'})
     response = admin_client.get(url)
     assert response.status_code == 200
     assert 'Are you sure you want to delete' in response.content.decode('utf-8')
